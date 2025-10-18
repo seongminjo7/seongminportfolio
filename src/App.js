2bf383d0b988d818bf412717d5e4fabe81da3d0a
jsm <0727jsm@gmail.com>
2025-10-18 16:13:30 +0900

    1018

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import GlobalStyle from './styles/GlobalStyle';  // GlobalStyle import
 import styled from 'styled-components';
 import ToggleBtn from './components/ToggleBtn';
@@ -9,12 +9,32 @@ import Design from './components/Design';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
+import Lenis from '@studio-freight/lenis';
+
 function App() {
+
+  const scrollContainerRef = useRef(null); // 스크롤을 적용할 컨테이너
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.8,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+
+    // 애니메이션 프레임을 업데이트하여 부드러운 스크롤을 지속시킵니다.
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    // 컴포넌트 언마운트 시 Lenis 인스턴스를 제거합니다.
+  }, []);
 
   return (
     <>
       <GlobalStyle />
-      <AllWrapper>
+      <AllWrapper ref={scrollContainerRef}>
         <Header />
         <ToggleBtn />
 
